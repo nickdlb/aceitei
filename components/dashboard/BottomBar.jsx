8b15308d37b1c35af96c7b17b275d6ffa653fb37
cassianoczn <cassiano_czn@live.com>
2025-01-03 22:07:28 -0300

    UI

--- components/dashboard/BottomBar.jsx
+++ components/dashboard/BottomBar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { UploadButton } from './UploadButton';
-import { PlusIcon } from '@heroicons/react/24/outline';
+    import { UploadButton } from './UploadButton';
+    import { PlusIcon } from '@heroicons/react/24/outline';
 
-const BottomBar = () => {
-  return (
-    <div className="flex items-center justify-center bg-white px-4 py-2 w-full">
-      <UploadButton text={<PlusIcon className="h-6 w-6" />} />
-    </div>
-  );
-};
+    const BottomBar = () => {
+      return (
+        <div className="flex items-center bg-white px-4 py-2 w-full pt-4 pb-4">
+          <UploadButton text={<PlusIcon className="h-6 w-6" />} />
+        </div>
+      );
+    };
 
-export default BottomBar;
+    export default BottomBar;
