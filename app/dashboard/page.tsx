73cb1eae9f2301a616db5bc9a898002f772288ec
Agencia-DLB <nicolasdiasleote@gmail.com>
2025-03-16 00:53:52 -0300

    Correção Erros Vercel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,1 +1,13 @@
- 
+'use client';
+
+import { ReactNode } from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
