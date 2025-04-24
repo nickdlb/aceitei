7746829b6135e8beea57107ff1d5de2ec0d09d5b
Agencia-DLB <nicolasdiasleote@gmail.com>
2025-04-23 22:33:58 -0300

    Update useImageCard.ts

--- hooks/useImageCard.ts
+++ hooks/useImageCard.ts
@@ -5,16 +5,41 @@ import { getImageUrl } from '@/utils/getImageUrl';
 import { useImages } from '@/contexts/ImagesContext';
 import { deleteCard } from '@/utils/deleteCard';
 import ImageProps from '@/types/ImageProps';
+import { useEffect } from 'react';
 
 export const useImageCard = (image: ImageProps, onDelete: (id: string) => void) => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [imageError, setImageError] = useState(false);
   const [showShareLink, setShowShareLink] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [title, setTitle] = useState(image.imageTitle || '');
+  const [documentTitle, setDocumentTitle] = useState('');
+  const [title, setTitle] = useState('');
   const router = useRouter();
   const imageUrl = getImageUrl(image.image_url);
   const { refreshImages } = useImages();
+
+  useEffect(() => {
+    const fetchDocumentTitle = async () => {
+      const { data, error } = await createSupabaseClient
+        .from('documents')
+        .select('title')
+        .eq('id', image.document_id)
+        .single();
+
+      if (error) {
+        console.error('Error fetching document title:', error);
+        setDocumentTitle('Sem título');
+        setTitle('Sem título');
+      }
+
+      if (data) {
+        setDocumentTitle(data.title || 'Sem título');
+        setTitle(data.title || 'Sem título');
+      }
+    };
+
+    fetchDocumentTitle();
+  }, [image.document_id]);
 
   const handleShare = async () => {
     const linkToShare = `${window.location.origin}/${image.document_id}`;
@@ -31,11 +56,16 @@ export const useImageCard = (image: ImageProps, onDelete: (id: string) => void) 
     if (!title) {
       setTitle('Sem título');
     }
-    try {
+    try {/*
       const { error } = await createSupabaseClient
         .from('pages')
         .update({ imageTitle: title })
         .eq('id', image.page_id);
+*/
+      const { error } = await createSupabaseClient
+        .from('documents')
+        .update({ title: title })
+        .eq('id', image.document_id);
 
       if (error) throw error;
       setIsEditing(false);
