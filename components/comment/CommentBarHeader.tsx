98e73157108e4c0b6c9bee3b17d2e1a984257b5c
Agencia-DLB <nicolasdiasleote@gmail.com>
2025-05-11 19:54:46 -0300

    Alterar texto pela img do logo

--- components/comment/CommentBarHeader.tsx
+++ components/comment/CommentBarHeader.tsx
@@ -9,7 +9,7 @@ const CommentHeader: React.FC<CommentHeaderProps> = ({ totalComments }) => {
       <div className="p-4 h-16">
         <div className=" items-center justify-between">
           <Link href="/dashboard" className="font-medium hover:text-acazul">
-            Feedybacky
+            <img src='/logo-feedybacky-dark.png' className='w-60'/>
           </Link>
         </div>
       </div>
