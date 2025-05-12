179f8db8f0d2234f06e4481d35d545220e0c899a
Agencia-DLB <nicolasdiasleote@gmail.com>
2025-05-12 04:09:25 -0300

    Update CommentBarFilterSite.tsx

--- components/comment/CommentBarFilterSite.tsx
+++ components/comment/CommentBarFilterSite.tsx
@@ -25,30 +25,21 @@ const CommentFilterSite: React.FC<CommentFilterSiteProps> = ({
 }) => {
   return (
     <div className="pt-4 pl-4 pb-3 space-y-3">
-      <div className="flex flex-wrap gap-2 items-center">
+      <div className="flex flex-col flex-wrap gap-2 items-start">
         <span className="text-acpreto font-semibold text-sm mr-2">
           Total: {totalComments}
         </span>
-        <button
-          onClick={() => setStatusFilter('ativo')}
-          className={`px-3 py-1 rounded-md text-sm ${statusFilter === 'ativo'
-            ? 'bg-yellow-500 text-acbrancohover'
-            : 'bg-acbg text-actextocinza'
-            }`}
-        >
-          Ativos
-        </button>
-        <button
-          onClick={() => setStatusFilter('resolvido')}
-          className={`px-3 py-1 rounded-md text-sm ${statusFilter === 'resolvido'
-            ? 'bg-green-500 text-acbrancohover'
-            : 'bg-acbg text-actextocinza'
-            }`}
-        >
-          Resolvidos
-        </button>
+        <div>
+          <button onClick={() => setStatusFilter('ativo')} className={`px-3 py-1 rounded-md text-sm ${statusFilter === 'ativo' ? 'bg-yellow-500 text-acbrancohover' : 'bg-acbg text-actextocinza'}`}>
+            Ativos
+          </button>
+          <button onClick={() => setStatusFilter('resolvido')} className={`px-3 py-1 rounded-md text-sm ${statusFilter === 'resolvido' ? 'bg-green-500 text-acbrancohover': 'bg-acbg text-actextocinza'}`}>
+            Resolvidos
+          </button>          
+        </div>
       </div>
       <div className="flex items-center gap-2">
+        { /* 
         <span className="text-acpreto font-semibold text-sm">Agrupar por:</span>
         <Select onValueChange={(value: 'Link' | 'Nenhum') => setGroupByFilter(value)} value={groupByFilter}>
           <SelectTrigger className="w-auto min-w-[120px]">
@@ -61,6 +52,7 @@ const CommentFilterSite: React.FC<CommentFilterSiteProps> = ({
             </SelectGroup>
           </SelectContent>
         </Select>
+        */ }
       </div>
     </div>
   );
